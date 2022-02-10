533a760ed2c10d90b918fbabe9054af110cad73e
keke <dondohare55@gmail.com>
2022-02-11 07:43:05 +0900

    halfway

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
 import type { NextPage } from 'next';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home: NextPage = () => {
+  const { data, error } = useSWR('http://localhost:4000/todos', fetcher);
+
+  if (error) return <>'An error has occurred.'</>;
+  if (!data) return <>'Loading...'</>;
+  console.log(data);
+
   return <div>Hello</div>;
 };
 
