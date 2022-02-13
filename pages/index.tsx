4fa7a5998ddf3c7e7d92e5d04b094d02db02966a
keke <dondohare55@gmail.com>
2022-02-13 21:31:14 +0900

    todos view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,31 @@
 import type { NextPage } from 'next';
 import useSWR from 'swr';
+import { Todo } from '../typing/Todo';
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const fetcher = (url: string) =>
+  fetch(url).then((res) => res.json() as Promise<Todo[]>);
 
-const Home: NextPage = () => {
+const Todos: NextPage = () => {
   const { data, error } = useSWR('http://localhost:4000/todos', fetcher);
 
   if (error) return <>'An error has occurred.'</>;
   if (!data) return <>'Loading...'</>;
-  console.log(data);
 
-  return <div>Hello</div>;
+  return (
+    <div>
+      {data.map((todo) => {
+        return (
+          <div>
+            <p>ID: {todo.id}</p>
+            <p>UserID: {todo.userId}</p>
+            <p>Title: {todo.title}</p>
+            <p>Content: {todo.content}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
-export default Home;
+export default Todos;
